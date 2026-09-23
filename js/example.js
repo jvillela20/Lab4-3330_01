704759c524eef47dc920ab4330a25cb6d72f7f0c
jvillela20 <jesus.villela01@utrgv.edu>
2026-09-23 13:23:08 -0500

    ul is now dynamic

--- js/example.js
+++ js/example.js
@@ -2,6 +2,13 @@ $(function() {
   
   // TODO: Create a list of JS Object each representing a game. 
   // Use the data contained in the <ul> to build this list of game titles.
+const games = [
+  { "title": "The Legend of Zelda: Breath of the Wild" },
+  { "title": "God of War Ragnarök" },
+  { "title": "Halo Infinite" },
+  { "title": "Minecraft" },
+  { "title": "Super Mario Odyssey" }
+];
 
 
   var gameList, newItemForm, newItemButton;
@@ -16,6 +23,15 @@ $(function() {
   // To do so, create a function that loops through each object in the game list, 
   // create a new node "list item" holding the game title and 
   // inject the new node inside the <ul>.
+  function loadGames(){
+    games.forEach(game => {
+      const listItem = $("<li>");
+      listItem.html(`${game.title}`);
+      gameList.append(listItem);
+    })
+
+  }
+ loadGames();
 
 
 
